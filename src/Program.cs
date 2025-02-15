bee106c1258d3fd61d84b7ec8a9114094568f4c5
MSI\samsonov <samsonovsa@loodsen.ru>
2025-02-15 13:46:46 +0300

    init

---
+++ src/Program.cs
@@ -0,0 +1,15 @@
+namespace RQL2Expression
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
